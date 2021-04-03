d8738febc374d5575a7089f9c5769de469f3fe6f
DilovanKoku <81458617+DilovanKoku@users.noreply.github.com>
2021-04-03 00:14:48 -0400

    Update Chapter2Question1.java

--- Chapter 2 Questions/Chapter 2 Question 01/Chapter2Question1.java
+++ Chapter 2 Questions/Chapter 2 Question 01/Chapter2Question1.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 
 public class Chapter2Question1 {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
+        
+        //Ask the user for temperature
         System.out.println("Enter temperature in Celsius: ");
         double temp = input.nextDouble();
+        
+        //Coversion
         double fahrenheit = (9.0/5.0 * temp + 32);
         System.out.println(temp + " degrees celsius is " + fahrenheit + " in degrees fahrenheit");
 
